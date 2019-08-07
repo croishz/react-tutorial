5c6004c532ef2e8f8a45453b6174c89e48d84dfa
lesh <gkdldpf@gmail.com>
2019-08-07 13:15:03 +0900

    add contact-app project

---
+++ contact-app/src/test.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Itembox from './component/Itembox';
+
+class Test extends Component {
+	render(){
+		return(
+			<Itembox></Itembox>
+		);
+	}
+}
+
+export default Test;
