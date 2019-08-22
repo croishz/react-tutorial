b085c9ebc90281b00e534be31eec29d1924db909
lesh <gkdldpf@gmail.com>
2019-08-22 11:56:11 +0900

    object merge issue

--- begin-react/src/component/ListBricks.js
+++ begin-react/src/component/ListBricks.js
@@ -1,44 +1,55 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useMemo, useRef} from 'react';
+
+const itemStyle = {
+	lineHeight : 1.5,
+	marginBottom : 14,
+	listStyleType : "square"
+}
+
+const btnStyle = {
+	background : "none",
+	border : "1px solid currentColor",
+	fontSize : 15,
+	color:"inherit",
+	padding : "4px 12px",
+	marginLeft : 10,
+}
+
+const uiElemStyle = {
+	cursor : "pointer"
+}
 
 function CreateBrick({username, address, changeEvent, createEvent, nameInput}){
 	return (
 		<div>
 			<input type="text" placeholder="ex. Sarah" name="username" value={username} onChange={changeEvent} ref={nameInput}/>
 			<input type="text" placeholder="ex. nth Avenue at nth St" name="address" value={address} onChange={changeEvent}/>
-			<button type="button" onClick={createEvent}>Add</button>
+			<button type="button" onClick={createEvent} style={Object.assign(btnStyle, uiElemStyle)}>Add</button>
 		</div>
 	);
 }
 
 function BrickItem({param, removeEvent, toggleEvent}){
-	const {id, username : name, address, status} = param;
+	const {id, username : name, address, leave} = param;
 
-	const itemStyle = {
-		lineHeight : 1.5,
-		marginBottom : 14
-	}
-	
-	const btnStyle = {
-		background : "none",
-		border : "1px solid currentColor",
-		fontSize : 15,
-		color:"inherit",
-		padding : "4px 12px",
-		marginLeft : 10,
-	}
-	
-	const uiElemStyle = {
-		cursor : "pointer"
-	}
 	const activeStyle = {
-		color : status === false && "crimson"
+		color : leave === true && "steelblue"
 	}
+	const btnStyleClone = JSON.parse(JSON.stringify(btnStyle));
+	const a = Object.assign(activeStyle, uiElemStyle);
+	const b = Object.assign(btnStyleClone, uiElemStyle);
+	console.log(activeStyle);	// 39행에서 변경.
+	console.log(uiElemStyle);
+	console.log(btnStyle);		// CreateBrick에서 변경
+	console.log(btnStyleClone);	// 40행에서 변경.
+
+
 	return(
 		<li data-index={id} style={itemStyle}>
 			<strong onClick={()=>toggleEvent(id)} style={Object.assign(activeStyle, uiElemStyle)}>{name}</strong>
 			{" leave in "}
 			<span>"{address}"</span>
-			<button type="button" onClick={()=>removeEvent(id)} style={Object.assign(btnStyle, uiElemStyle)}>Delete</button>
+			<button type="button" onClick={()=>removeEvent(id)} style={Object.assign(btnStyleClone, uiElemStyle)}>Delete</button>
 		</li>
 	)
 }
@@ -57,43 +68,49 @@ function ListBrick({userInfo, onRemove, onActive}){
 		</ul>
 	)
 }
+function Count({count}){
+	return(
+		<span>{count}</span>
+	);
+}
+
+const states = {
+	inputs : {
+		username : '',
+		address : ''
+	},
+	information : [
+		{
+			id : 1,
+			username : "Corina",
+			address : "West 9th Avenue at 24th streets",
+			leave : true 	
+		},
+		{
+			id : 2,
+			username : "Kevin",
+			address : "East 5th Avenue at 10th streets",
+			leave : false 	
+		},
+		{
+			id : 3,
+			username : "Timothy",
+			address : "West 12th Avenue at 1st streets",
+			leave : false 	
+		},
+		{
+			id : 4,
+			username : "Sarapova",
+			address : "West 12th Avenue at 2nd streets",
+			leave : true 	
+		},
+	]
+}
 
 function ListBricks(){
-	const [state, setUpdate] = useState({	// useState는 scope 외부의 값을 참조시킬 수 없다. 
-		inputs : {
-			username : '',
-			address : ''
-		},
-		information : [
-			{
-				id : 1,
-				username : "Corina",
-				address : "West 9th Avenue at 24th streets",
-				status : true 	
-			},
-			{
-				id : 2,
-				username : "Kevin",
-				address : "East 5th Avenue at 10th streets",
-				status : false 	
-			},
-			{
-				id : 3,
-				username : "Timothy",
-				address : "West 12th Avenue at 1st streets",
-				status : false 	
-			},
-			{
-				id : 4,
-				username : "Sarapova",
-				address : "West 12th Avenue at 2nd streets",
-				status : true 	
-			},
-		]
-	});
+	const [state, setUpdate] = useState(states);
 	const {inputs, information : userInfo} = state;
 	const {username, address} = inputs;
-	// console.log("state : ", state);	
 
 	const onChange = (event)=>{
 		const {name, value} = event.target;
@@ -112,23 +129,21 @@ function ListBricks(){
 		const newCitizen = {
 			id : nextId.current,
 			username,
-			address
+			address,
+			leave : true
 		};
 		const [arr1, arr2, ...rest] = userInfo;
 		setUpdate({
 			...state,
-			inputs : {
-				...inputs,
-				username : '',
-				address : ''
-			},
+			inputs : states.inputs,
 			information : [	// key name 주의
 				// ...userInfo,
 				// newCitizen
 				arr1, arr2, newCitizen, ...rest
 			]
 		});
-		// console.log(newCitizen, state);
+		// console.log("newCitizen : ", newCitizen);
+		// console.log("state : ", state);
 		nextId.current += 1;
 		nameInput.current.focus();
 	}
@@ -137,25 +152,28 @@ function ListBricks(){
 		setUpdate({
 			...state,
 			information : userInfo.filter( info => info.id !== id )
-		})
+		});
 		// nextId.current -= 1;
 	}
 	const onActive = (id)=>{
-		console.log("id : ", id);
+		// console.log("id : ", id);
 		setUpdate(
-			userInfo.map(
+			{...state,
+			information : userInfo.map(
 				info => info.id === id ?
-				{...state, 
-					information : [
-						...userInfo, 
-						userInfo[info.id - 1].status = !info.status
-					]
-				} :
-				info,
-				console.log(state)
+				(
+				// console.log("match"),
+				{...info, leave:!info.leave}) :
+				(
+				// console.log("unmatch"),
+				info
+				)
 			)
-		)
+		});
 	}
+
+	const activebrickItemCount = useMemo(()=> userInfo.filter(info => info.leave).length, [userInfo]);
+
 	const blockStyle = {
 		backgroundColor:"lightcoral", 
 		color:"#fefefe",
@@ -171,6 +189,7 @@ function ListBricks(){
 				nameInput={nameInput}
 			/>
 			<ListBrick userInfo={userInfo} onRemove={onRemove} onActive={onActive}/>
+			<Count count={activebrickItemCount}/>
 		</div>
 	);
 }
