e9a7efbc6fceeb403695eb8daf7f31e26f9b1ea4
lesh <gkdldpf@gmail.com>
2019-08-23 14:59:18 +0900

    create refactoring

--- begin-react/src/component/CompositionList.js
+++ begin-react/src/component/CompositionList.js
@@ -33,15 +33,38 @@ function List({accounts}){
 	)
 }
 
-function CreateList({user, email, changeAction, createAction}){
+function CreateList({accounts}){
+	const actionDispatch = useContext(eventAction);
+	const [{user, email}, onChange, onReset, onRefresh] = useInputsByReduce({
+		user : '',
+		email : '',
+	});
+	const nextId = useRef(accounts.length + 1);
+	const onCreate = useCallback(()=>{
+		actionDispatch({
+			type : "CREATE_ACCOUNT",
+			account : {
+				id : nextId.current,
+				username : user,
+				email,
+			}
+		});
+		nextId.current += 1;
+		onRefresh();
+	},[actionDispatch,  onRefresh, email, user])
 	return(
 		<div>
-			<input type="text" name="user" value={user} onChange={changeAction} />
-			<input type="text" name="email" value={email} onChange={changeAction} />
-			<button type="button" onClick={createAction}>Account Regist</button>
+			<input type="text" name="user" value={user} onChange={onChange} />
+			<input type="text" name="email" value={email} onChange={onChange} />
+			<button type="button" onClick={onCreate}>Account Regist</button>
 		</div>
 	);
-}
+} 
+/* 
+	chapter 22의 예제 https://codesandbox.io/s/begin-react-b7c16?fontsize=14
+	보편성을 위해 nextId를 state.accounts의 length로부터 받아오는 것이 문제.
+	state도 같이 context value로 받아야 한다.
+*/
 
 // state
 const initialState = {
@@ -93,14 +116,15 @@ function reducer(state, action){
 			// let [arr1, arr2, ...rest] = action.newAccounts;	새 배열을 참조, 전달받아 사용하나 기존 값을 사용하나 동일. state parameter 자체가 변경값이라서 어느 쪽이든 결과값은 같다.
 			// let [arr1, arr2, ...rest] = initialState.accounts; state로 관리되기 이전의 초기값으로 다루므로 length 변화없이 push되는 action.account만 계속 바뀐다. 	
 			// let [arr1, arr2, ...rest] = state.accounts;
+			console.log(action.account);
 			return({
-				...state,
-				// formValue : initialState.formValue,
-				// accounts : state.accounts.concat(action.account)	// composite
-				accounts : [										// first, last insert
-					action.account,
-					...state.accounts,
-				]
+				formValue : initialState.formValue,
+				accounts : state.accounts.concat(action.account)	// composite
+				// ...state,
+				// accounts : [										// first, last insert
+				// 	action.account,
+				// 	...state.accounts,
+				// ]
 				// accounts : [										// any position
 				// 	arr1, arr2, action.account, ...rest
 				// ]
@@ -131,53 +155,16 @@ function reducer(state, action){
 function CompositionList(){
 	const [state, dispatch] = useReducer(reducer, initialState);
 	const {accounts} = state;
-	// const {user, email} = formValue;
-	const [form, onChange, onReset, onRefresh] = useInputsByReduce({
-		user : '',
-		email : '',
-	});
-	const {user, email} = form;
-	const nextId = useRef(accounts.length + 1);
-
-	// const onChange = useCallback((event) => {
-	// 	const {name, value} = event.target;
-	// 	dispatch({
-	// 		type : "CHNAGE_ACCOUNT",
-	// 		name, 
-	// 		value,
-	// 	})
-	// }, []);
-
-	const onCreate = useCallback(()=>{
-		dispatch({
-			type : "CREATE_ACCOUNT",
-			accounts, 
-			// newAccounts : [...accounts],
-			account : {
-				id : nextId.current,
-				username : user,
-				email,
-				// active : true
-			}
-		});
-		nextId.current += 1;
-		// onReset();
-		onRefresh();
-	},[user, email, accounts, onReset]);
 
 	const count = useMemo(()=>{
 		return(
 			accounts.filter( account=> account.active).length
 		);
 	}, [accounts]);
+
 	return(
 		<eventAction.Provider value={dispatch}>
-			<CreateList 
-				user={user} 
-				email={email} 
-				changeAction={onChange} 
-				createAction={onCreate}
-			/>
+			<CreateList accounts={accounts}/>
 			<List accounts={accounts} />
 			{count}
 		</eventAction.Provider>
