632ac6b8aff5e212da48d86def7da22ea54ca3a1
lesh <gkdldpf@gmail.com>
2019-08-30 17:22:51 +0900

    refresh + styling set

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,126 +1,22 @@
-import React, {useReducer, createContext, useCallback, useRef} from "react";
-import CreateTodoList from "./component/CreateTodoList";
-import List from "./component/List";
-import DateWindow from "./component/DateWindow";
+import React from "react";
+import styled from "styled-components";
 
-const initialState = {
-	input: {
-		todoText: "",
-	},
-	todo: [
-		{
-			id: 1,
-			todoText: "아침 산책",
-			check: false,
-		},
-		{
-			id: 2,
-			todoText: "개고양이 먹이 주기",
-			check: false,
-		},
-		{
-			id: 3,
-			todoText: "아침 먹기",
-			check: false,
-		},
-		{
-			id: 4,
-			todoText: "집문 닫기",
-			check: false,
-		},
-	],
-};
-function reducer(state, action) {
-	switch (action.type) {
-		case "CREATE_TODO":
-			console.log(action.addTodo);
-			return {
-				...state,
-				todo: state.todo.concat(action.addTodo),
-			};
-		case "REMOVE_TODO":
-			if (state.todo.length === 0) {
-				return {
-					...state,
-					todo: [{id: 1, todoText: "", check: false}],
-				};
-			} else {
-				return {
-					...state,
-					todo: state.todo.filter(
-						todoinfo => todoinfo.id !== action.id,
-					),
-				};
-			}
-		case "DONE_TODO":
-			console.log(action.id);
-			return {
-				...state,
-				todo: state.todo.map(todoinfo =>
-					todoinfo.id === action.id
-						? {...todoinfo, check: !todoinfo.check}
-						: todoinfo,
-				),
-			};
-		case "CHANGE_VALUE":
-			console.log(action.value);
-			return {
-				...state,
-				input: {
-					todoText: action.value,
-				},
-			};
-		case "MATCHCASE_TODO":
-			console.log(action.input.todoText);
-			return {
-				state,
-			};
-		default:
-			throw new Error("non specification action");
-	}
-}
+const Icon = styled(
+    ({ component, ...props }) => React.cloneElement(component, props)
+)`
+  background: red;
+`
+
+const elem = "h4";
+const Title = styled(({props})=> React.createElement(elem))`
+	font-size : 20px;
+	line-height : 18px;
+	color: tan;
+`
 function App() {
-	const [{input, todo}, dispatch] = useReducer(reducer, initialState);
-
-	const onChange = useCallback(event => {
-		const {value} = event.target;
-		dispatch({
-			type: "CHANGE_VALUE",
-			value,
-		});
-	}, []);
-
-	const onMatch = useCallback(() => {
-		dispatch({
-			type: "MATCHCASE_TODO",
-			input,
-		});
-	}, [input]);
-
-	const nextId = useRef(todo.length + 1);
-	const onCreate = useCallback(() => {
-		const addTodo = {
-			id: nextId.current,
-			todoText: input.todoText,
-			check: false,
-		};
-		dispatch({
-			type: "CREATE_TODO",
-			addTodo,
-		});
-		nextId.current += 1;
-	}, [input.todoText]);
-	return (
-		<useDispatch.Provider value={dispatch}>
-			<DateWindow />
-			<List todo={todo} />
-			<CreateTodoList
-				input={input}
-				changeAction={onChange}
-				createAction={onCreate}
-			/>
-		</useDispatch.Provider>
+	return(
+		<Title>Hello world!</Title>
 	);
 }
-export const useDispatch = createContext("default");
+
 export default App;
---
+++ todo-list/src/component/styling_test/ModuleCss.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./sample.module.scss"
+
+import React from "react";
+
+function ModuleCss() {
+	return(
+		<button>Button</button>
+	);
+}
+
+export default ModuleCss;
