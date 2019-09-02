21d6a3bf959cacaccc0e3e5959e05717e9282ce5
lesh <gkdldpf@gmail.com>
2019-09-02 14:36:37 +0900

    use font awesome icon

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import ModuleCass from "../src/component/styling_test/ModuleCss"
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ModuleCass />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
