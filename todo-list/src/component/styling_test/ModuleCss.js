0d72f9213084f18d4b3896fb4a0df97d7bb68b37
lesh <gkdldpf@gmail.com>
2019-09-02 14:36:12 +0900

    module css sample

--- todo-list/src/component/styling_test/ModuleCss.js
+++ todo-list/src/component/styling_test/ModuleCss.js
@@ -1,11 +1,44 @@
-import React from "react";
-import styles from "./sample.module.scss"
+import React, { useState } from "react";
+import styles from "./sample.module.scss";
+import classnames from "classnames";
 
-import React from "react";
+// icon 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fas, far);
+
+// component
+function Checkbox({ children }) {
+	const [check, setCheck] = useState(false);
+	const onCheck = (event) => {
+		setCheck(event.target.checked);
+		// console.log(event.target.checked);
+	}
+	const oncheck_byClick = () => {
+		setCheck(!check);
+		// console.log(check);
+	}
+	return (
+		// no include input
+		<div className={classnames(styles.checkbox)}>
+			<input type="checkbox" id="checkbox1" checked={check} onChange={onCheck} />
+			<FontAwesomeIcon className={classnames(styles.uiElements)} icon={check ? ["fas", "check-square"] : ["far", "square"]} onClick={oncheck_byClick} />
+			<label className={classnames(styles.uiElements)} htmlFor="checkbox1" aria-label={children}>{children}</label>
+		</div>
+	);
+}
 
 function ModuleCss() {
-	return(
-		<button>Button</button>
+	return (
+		<>
+			<button
+				className={classnames(styles.button_box, styles.uiElements)}
+			>Button class is, <em>{styles.button_box}</em>
+			</button>
+			<Checkbox>가입에 동의합니다.</Checkbox>
+		</>
 	);
 }
 
